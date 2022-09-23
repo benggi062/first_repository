eae19b07a677d5048d0a4fcff4be40a13a9bbf62
benggi062 <rldnd0616@naver.com>
2022-09-23 16:26:32 +0900

    four_commit_file

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,5 @@
-def add(x,y):
-    a=x+y
-    return a
+def print_hello():
+    print("Hello World!!")
 
-def print_KIT():
-    print('KIT')
-
-if __name__=='__main__':
-    print_KIT()
-    print(add(1,2))
-    print('hi')
+if __name__ == '__main__':
+    print_hello()
