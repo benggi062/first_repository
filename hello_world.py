a99c7181f2f868868cadd9675ca88cf3b778cf5d
benggi062 <rldnd0616@naver.com>
2022-09-23 16:06:29 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,12 @@
-def print_hello():
-    print("Hello World!!")
 
-if __name__ == '__main__':
-    print_hello()
-print("김븅슨")
+
+def add(x,y):
+    a=x+y
+    return a
+
+def print_KIT():
+    print('KIT')
+
+if __name__=='__main__':
+    print_KIT()
+    print(add(1,2))
